bde16ba41c6ae4c0d579bcbc7931db21211ce594
Gun Gun Priatna <gungunpriatna02@gmail.com>
2019-10-26 08:49:50 +0700

    add hello.php

---
+++ code/pertemuan_2/hello.php
@@ -0,0 +1,18 @@
+<?php
+//ini menampilkan tulisan
+echo "<center>selamat belajar pemrograman dasar </center>"; //ini komentar
+
+# ini tag break
+echo "<br>";
+
+/*
+* ini komentar pertama
+* komentar kedua
+* komentar ketiga
+**/
+
+echo 555;
+
+echo "<br>";
+
+echo "selamat belajar <br> pemrograman dasar";
