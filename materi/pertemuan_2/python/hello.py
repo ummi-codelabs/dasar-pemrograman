8f6eab5aae9409992dcf134ff91eef0a1132755c
Gun Gun Priatna <gungunpriatna02@gmail.com>
2021-10-03 06:18:13 +0700

    add python sample code. related #25

---
+++ materi/pertemuan_2/python/hello.py
@@ -0,0 +1,16 @@
+# ini menampilkan tulisan
+print("selamat belajar pemrograman dasar") # ini komentar
+
+# ini garis baru
+print("\n")
+
+# ini komentar pertama
+# komentar kedua
+# komentar ketiga
+
+print(555)
+
+print("\n")
+
+print("selamat belajar \n pemrograman dasar")
+
