688bec98f85c449f4f03f8a85d0977dd9a7a38da
luffy <sayaa.luffy@gmail.com>
2021-10-09 17:54:55 +0700

    add sample code java untuk pertemuan 7

---
+++ materi/pertemuan_7/java/LoopWhile3.java
@@ -0,0 +1,10 @@
+public class LoopWhile3 {
+    public static void main(String[] args) {
+        int i = 100;
+
+        while(i > 0) {
+            System.out.println(i);
+            i = i - 5;
+        }
+    }
+}
