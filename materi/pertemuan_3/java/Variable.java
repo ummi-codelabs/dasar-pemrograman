4f64f68c991adbf0e210e0f4932cab88912233c5
luffy <sayaa.luffy@gmail.com>
2021-10-09 19:28:41 +0700

    check materi pertemuan 3. related #37

--- materi/pertemuan_3/java/Variable.java
+++ materi/pertemuan_3/java/Variable.java
@@ -1,18 +1,23 @@
 
 public class Variable {
     public static void main(String[] args) {
+
+        // deklarasi variable
         String nama_variable = "ini string";
         String nama_mahasiswa = "Resita";
         int nilai = 100;
 
-        boolean ini_boolean = true;
+        //deklarasi variable tipe data boolean
+        boolean boolean_var = true;
         boolean ini_boolean_false = false;
-        float ini_float = 0.05f;
+
+        // deklarasi variable tipe data float
+        float float_var = 0.05f;
 
         System.out.println(nama_mahasiswa);
         System.out.println(nilai);
-        System.out.println(ini_boolean);
-        System.out.println(ini_float);
+        System.out.println(boolean_var);
+        System.out.println(float_var);
 
         nilai = 98;
         System.out.println(nilai);
