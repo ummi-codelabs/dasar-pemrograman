ea08aab52dc8278e9db3f8a58e9c4bc5ce0192fd
aira novitasari <recodelabsdotcom@gmail.com>
2021-10-09 16:08:35 +0700

    add sample kode java untuk pertemuan 3

---
+++ materi/pertemuan_3/java/Konstanta.java
@@ -0,0 +1,7 @@
+public class Konstanta {
+    public static final double PHI = 3.14;
+
+    public static void main(String[] args) {
+        System.out.println(PHI);
+    }
+}
---
+++ materi/pertemuan_3/java/Variable.java
@@ -0,0 +1,20 @@
+
+public class Variable {
+    public static void main(String[] args) {
+        String nama_variable = "ini string";
+        String nama_mahasiswa = "Resita";
+        int nilai = 100;
+
+        boolean ini_boolean = true;
+        boolean ini_boolean_false = false;
+        float ini_float = 0.05f;
+
+        System.out.println(nama_mahasiswa);
+        System.out.println(nilai);
+        System.out.println(ini_boolean);
+        System.out.println(ini_float);
+
+        nilai = 98;
+        System.out.println(nilai);
+    }
+}
