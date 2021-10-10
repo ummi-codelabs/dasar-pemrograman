3faf30d3cc5de00fd32b6bf547bcf4cb451e5fae
Gun Gun Priatna <gungunpriatna02@gmail.com>
2021-10-10 07:17:49 +0700

    add sample code python untuk pertemuan 4

---
+++ materi/pertemuan_4/python/hitung_volume.py
@@ -0,0 +1,5 @@
+panjang = int(raw_input("masukan panjang: "))
+lebar = int(raw_input("masukan lebar: "))
+tinggi = int(raw_input("masukan tinggi: "))
+volume = panjang * lebar * tinggi
+print(volume)
