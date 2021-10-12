ac0f13c7309444d5d5aa34b5cf67ddd18c5b3e05
Nadia <nadia.debbiepra@gmail.com>
2021-10-12 21:38:58 +0700

    add: basic syntax

---
+++ materi/pertemuan_2/js/hello.js
@@ -0,0 +1,17 @@
+// ini menampilkan tulisan
+console.log("selamat belajar pemrograman dasar"); // ini komentar
+
+// ini new line
+console.log("\n");
+
+/*
+ini komentar pertama
+komentar kedua
+komentar ketiga
+*/
+
+console.log(555);
+
+console.log("\n");
+
+console.log("Selamat belajar \n pemrograman dasar");
