5f2bc23c4ae7accbc80d6080cf146564159b2f96
Nadia <nadia.debbiepra@gmail.com>
2021-10-13 06:44:59 +0700

    feat: add sample code if-else

---
+++ materi/pertemuan_5/js/login.js
@@ -0,0 +1,8 @@
+let username = 'admin';
+let password = 'garahasia';
+
+if (username == 'admin' && password == 'rahasia') {
+    console.log('User berhasil login');
+} else {
+    console.log('User gagal login');
+}
