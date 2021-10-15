27463f10b9d5b249a51e19f1cc1a3c831b985517
Gun Gun Priatna <gungunpriatna02@gmail.com>
2021-10-15 10:40:48 +0700

    feat: add sample code variable

---
+++ materi/pertemuan_3/go/variable.go
@@ -0,0 +1,27 @@
+package main
+import ("fmt")
+
+func main() {
+
+	//deklarasi variable
+	var nama_variable string = "ini string"
+	var nama_mahasiswa string = "Resita"
+	var nilai int = 100
+
+	// deklarasi variable
+	var boolean_var bool = true
+	var ini_boolean_false = false
+
+	// deklarasi variable
+	var float_var float32 = 0.05
+
+	fmt.Println(nama_variable)
+	fmt.Println(nama_mahasiswa)
+	fmt.Println(nilai)
+	fmt.Println(boolean_var)
+	fmt.Println(ini_boolean_false)
+	fmt.Println(float_var)
+
+	nilai = 98
+	fmt.Println(nilai)
+}
