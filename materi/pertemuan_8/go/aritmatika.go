3ccc8c3e26bea33b630cdd3d3740710dc2f239c2
Gun Gun Priatna <gungunpriatna02@gmail.com>
2021-10-16 07:50:25 +0700

    feat: use another file in golang

---
+++ materi/pertemuan_8/go/aritmatika.go
@@ -0,0 +1,21 @@
+package main
+
+func penjumlahan(a int, b int) int {
+	return a + b
+}
+
+func pengurangan(a int, b int) int {
+	return a - b
+}
+
+func perkalian(a int, b int) int {
+	return a * b
+}
+
+func pembagian(a int, b int) int {
+	return a / b
+}
+
+func hasilBagi(a int, b int) int {
+	return a % b
+}
