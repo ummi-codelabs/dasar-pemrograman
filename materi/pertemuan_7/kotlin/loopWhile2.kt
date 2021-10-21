6d3c600868a24f33dfbfbc1c97c77c5826b4f4ef
Reas-19 <Rezaalamsyah96@ummi.ac.id>
2021-10-21 22:21:43 +0700

    Add kotlin pada pertemuan_7

---
+++ materi/pertemuan_7/kotlin/loopWhile2.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var i = 1
+
+    while (i <= 10) {
+        println(i)
+        i += 2
+    }
+}
---
+++ materi/pertemuan_7/kotlin/loopWhile3.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var i = 100
+
+    while (i > 0) {
+        println(i)
+        i -= 5
+    }
+}
