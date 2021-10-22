24fdd016ac53c85295096a18d0c757df4557c60d
Reas-19 <Rezaalamsyah96@ummi.ac.id>
2021-10-22 15:01:42 +0700

    Add kotlin ke pertemuan_8

---
+++ materi/pertemuan_8/kotlin/Aritmatika.kt
@@ -0,0 +1,23 @@
+class Aritmatika {
+
+    fun penjumlahan(a: Int, b: Int): Double {
+        return (a + b).toDouble()
+    }
+
+    fun pengurangan(a: Int, b: Int): Double {
+        return (a - b).toDouble()
+    }
+
+    fun perkalian(a: Int, b: Int): Double {
+        return (a * b).toDouble()
+    }
+
+    fun pembagian(a: Int, b: Int): Double {
+        return (a / b).toDouble()
+    }
+
+    fun hasilBagi(a: Int, b: Int): Double {
+        return (a % b).toDouble()
+    }
+
+}
